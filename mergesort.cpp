a23a4bc0378954afd80d2ded0838d99cd3452afb
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-04-15 10:15:24 +0700

    step 1

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
 
+ //create main a
+ int arr[20], B[20];
+ int n;
+
+
 void mergeSort(int low, int high)
 {
    
-    int arr[20], B[20];
-
+   if (low >= high)
+   {
+        return;
+   }
+   
+   
 }
